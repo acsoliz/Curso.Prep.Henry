b54a1d86b6ca0560a9044175a42078fd36d1b35e
acsoliz <acsoliz@outlook.com>
2022-03-24 23:46:33 +0100

    homework 07 HHs

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -1,79 +1,76 @@
 // Do not change any of the function names
 
 function mayuscula(nombre) {
-  //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
-  //ej: Recibe "mario" ----> Devuelve "Mario"
-  //Tu código:
-
-  return nombre.charAt(0).toUpperCase()+nombre.slice(1);
-
+	//La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
+	//ej: Recibe "mario" ----> Devuelve "Mario"
+	//Tu código:
 }
 
 function invocarCallback(cb) {
-  // Invoca al callback `cb`
-  //Tu código:
-  return cb();
+	// Invoca al callback `cb`
+	//Tu código:
 }
 
 function operacionMatematica(n1, n2, cb) {
-  //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
-  //Devolver el callback pasándole como argumentos los números recibidos.
-  //Tu código:
-  return cb (n1,n2)
+	//Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
+	//Devolver el callback pasándole como argumentos los números recibidos.
+	//Tu código:
 }
 
 function sumarArray(numeros, cb) {
-  // Suma todos los números enteros (int/integers) de un array ("numeros")
-  // Pasa el resultado a `cb`
-  // No es necesario devolver nada
-  //Tu código:
-
-  var suma=numeros.reduce(function(acc, cur ){
-    return acc+cur;
-  },0);
-  cb(suma);
+	// Suma todos los números enteros (int/integers) de un array ("numeros")
+	// Pasa el resultado a `cb`
+	// No es necesario devolver nada
+	//Tu código:
 }
 
 function forEach(array, cb) {
-  // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
-  // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
-  //Tu código:
-array.forEach(function (elemento){
-  cb(elemento);
-})
+	// Itera sobre la matriz "array" y pasa los valores al callback uno por uno
+	// Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+	///[1, 2, 3, 4, 5, 7]
+	//[{}, {}, ...]
+	//Tu código:
+
+	array.forEach(function(element) {
+		cb(element);
+	});
 }
 
 function map(array, cb) {
-  // Crea un nuevo array
-  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
-  // El nuevo array debe tener la misma longitud que el array del argumento
-  //Tu código:
-  var nuevoArray = array.map(function(x){
-    return cb(x);
-  })
-  return nuevoArray;
+	// Crea un nuevo array
+	// Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
+	// El nuevo array debe tener la misma longitud que el array del argumento
+	//Tu código:
+	var nuevoArray = [];
+	nuevoArray = array.map(function(element) { // ()=>{}
+		return cb(element);
+	});
+	return nuevoArray;
 }
 
 function filter(array) {
-  //Filtrar todos los elementos del array que comiencen con la letra "a".
-  //Devolver un nuevo array con los elementos que cumplen la condición
-  //Tu código:
-  var newArray = array.filter(function(elemento){
-    return elemento[0]==='a'
-  })
-  return newArray;  
-}
+	//Filtrar todos los elementos del array que comiencen con la letra "a".
+	//Devolver un nuevo array con los elementos que cumplen la condición
+	//Tu código:
+	 // [aaa, , asss, ddd, dfe, ae, ] => 
+	var filtrados = array.filter(function(element){
+		// console.log(element)
+		return element[0] === "a" 
+	
+	})
+	return filtrados //[aaa, , asss, ae ]
 
+}
 
 // No modificar nada debajo de esta línea
 // --------------------------------
 
 module.exports = {
-  mayuscula,
-  invocarCallback,
-  operacionMatematica,
-  sumarArray,
-  forEach,
-  map,
-  filter
+	mayuscula,
+	invocarCallback,
+	operacionMatematica,
+	sumarArray,
+	forEach,
+	map,
+	filter
 };
